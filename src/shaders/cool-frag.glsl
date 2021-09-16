7aec08adcb7a899ce6724dd8c1e377fe2775cc95
Emma Holthouser <emma20@seas.upenn.edu.com>
2021-09-15 21:06:16 -0400

    updated ambient light again

--- src/shaders/cool-frag.glsl
+++ src/shaders/cool-frag.glsl
@@ -147,7 +147,7 @@ void main()
         // Avoid negative lighting values
         // diffuseTerm = clamp(diffuseTerm, 0, 1);
 
-        float ambientTerm = 0.1;
+        float ambientTerm = 0.2;
         diffuseTerm = clamp(diffuseTerm, 0.f, 1.f);
         float lightIntensity = diffuseTerm + ambientTerm;   //Add a small float value to the color multiplier
                                                             //to simulate ambient lighting. This ensures that faces that are not
